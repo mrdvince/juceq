60b07a9fce000deb3511814108af93b958fae067
mrdvince <vincivenv@gmail.com>
2021-06-12 13:37:32 +0300

    fix: Linking, include juce_dsp

--- Plugins/juceq/Source/Parameters.h
+++ Plugins/juceq/Source/Parameters.h
@@ -2,7 +2,6 @@
 
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_dsp/juce_dsp.h>
-#include <juce_core/juce_core.h>
 
 struct Parameters {
     void add(juce::AudioProcessor &processor) const {

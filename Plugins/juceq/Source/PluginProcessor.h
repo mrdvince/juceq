9d8f4b3e3bd19ece889e853bf60f8ec9f4ab571e
mrdvince <vincivenv@gmail.com>
2021-06-12 23:08:08 +0300

    feat: Filter, Connect low cut params

--- Plugins/juceq/Source/PluginProcessor.h
+++ Plugins/juceq/Source/PluginProcessor.h
@@ -2,10 +2,17 @@
 
 #include "Parameters.h"
 
+enum Slope {
+    Slope_12,
+    Slope_24,
+    Slope_36,
+    Slope_48
+};
+
 struct ChainSettings {
     float peakFreq{0}, peakGainInDecibels{0}, peakQuality{1.f};
     float lowCutFreq{0}, highCutFreq{0};\
-    int lowCutSlope{0}, highCutSlope{0};
+    Slope lowCutSlope{Slope::Slope_12}, highCutSlope{Slope::Slope_12};
 };
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState &apvts);
@@ -69,7 +76,7 @@ private:
     MonoChain leftChain, rightChain;
 
     enum ChainPositions {
-        lowCut, Peak, HighCut
+        LowCut, Peak, HighCut
     };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(JuceqAudioProcessor)

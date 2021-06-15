df8c6f2f3e6ccdf55471a5642d0ed4af2c1053e7
mrdvince <vincivenv@gmail.com>
2021-06-15 09:28:38 +0300

    feat: UI, Add sliders

--- Plugins/juceq/Source/PluginEditor.h
+++ Plugins/juceq/Source/PluginEditor.h
@@ -2,6 +2,13 @@
 
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider {
+    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                        juce::Slider::TextEntryBoxPosition::NoTextBox) {
+
+    }
+};
+
 class JuceqAudioProcessorEditor : public juce::AudioProcessorEditor {
 public:
     JuceqAudioProcessorEditor(JuceqAudioProcessor &);
@@ -16,6 +23,15 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     JuceqAudioProcessor &audioProcessor;
+    CustomRotarySlider peakFreqSlider,
+            peakGainSlider,
+            peakQualitySlider,
+            lowCutFreqSlider,
+            highCutFreqSlider,
+            lowCutSlopeSlider,
+            highCutSlopeSlider;
+
+    std::vector<juce::Component *> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (JuceqAudioProcessorEditor)
 };
